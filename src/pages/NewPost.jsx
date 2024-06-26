b3a902bc89fb98e13063471b26f4b91da378ccb5
theDuyet-Shine <nguyenduyethp.working@gmail.com>
2024-06-27 06:56:43 +0700

    fix

--- src/pages/NewPost.jsx
+++ src/pages/NewPost.jsx
@@ -35,14 +35,22 @@ const NewPost = () => {
 
   const handleSubmit = async (e) => {
     e.preventDefault();
-    const author = userAuth.user._id;
-    const newPost = { title, content, tags, author };
-    try {
-      const response = await api.post("/post", newPost);
-      if (response.status === 201) toast.success("Đăng bài thành công!");
-      setTimeout(() => navigate(`/post/${response.data._id}`), 2000); // Điều hướng sau khi đăng bài
-    } catch (error) {
-      toast.error("Lỗi khi đăng bài!");
+    if (content.length === 0) {
+      toast.error("Không được để trống nội dung");
+    } else {
+      if (title.length === 0) {
+        toast.error("Không được để trống tiêu đề");
+      } else {
+        const author = userAuth.user._id;
+        const newPost = { title, content, tags, author };
+        try {
+          const response = await api.post("/post", newPost);
+          if (response.status === 201) toast.success("Đăng bài thành công!");
+          setTimeout(() => navigate(`/post/${response.data._id}`), 2000); // Điều hướng sau khi đăng bài
+        } catch (error) {
+          toast.error("Lỗi khi đăng bài!");
+        }
+      }
     }
   };
 
